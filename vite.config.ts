e39bc06f556f20b57aeab51104408eb200c57d28
Francois Barrailla <francois.barrailla@gmail.com>
2026-03-22 08:59:18 +0700

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,8 @@ export default defineConfig(({mode}) => {
       hmr: process.env.DISABLE_HMR !== 'true',
       proxy: {
         '/api': 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
       },
     },
     build: {
